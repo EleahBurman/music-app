7de174626b0ee8acb0286e78ef705efc10f76b55
eleahburman <eleahjburman@gmail.com>
2023-10-04 13:38:19 -0400

    Create first view

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
 # Create your views here.
 def main(request):
-  return HttpResponse("Hello")
+  return HttpResponse("<h1>Hi</h1>")
